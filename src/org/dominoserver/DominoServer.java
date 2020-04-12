00c3ac64a4667abbf9095cfcecc078766d44f033
Gonzalo Pesquero <gpesquero@yahoo.es>
2020-04-12 13:57:31 +0200

    First commit

---
+++ src/org/dominoserver/DominoServer.java
@@ -0,0 +1,17 @@
+package org.dominoserver;
+
+public class DominoServer {
+
+	private static String APP_NAME="DominoServer";
+	
+	public static void main(String[] args) {
+		
+		Log.info("Starting <"+APP_NAME+">...");
+		
+		
+		// TODO Auto-generated method stub
+		
+		Log.info("Quitting <"+APP_NAME+">");
+
+	}
+}
---
+++ src/org/dominoserver/Log.java
@@ -0,0 +1,28 @@
+package org.dominoserver;
+
+import java.time.LocalDateTime;
+
+public class Log {
+	
+	private static void log(String type, String text) {
+		
+		LocalDateTime time=LocalDateTime.now();
+		
+		String timeString=String.format("%04d-%02d-%02d %02d:%02d:%02d",
+				time.getYear(),
+				time.getMonthValue(),
+				time.getDayOfMonth(),
+				time.getHour(),
+				time.getMinute(),
+				time.getSecond());
+		
+		System.out.println(timeString+" > "+type+": "+text);
+		
+	}
+	
+	public static void info(String text) {
+		
+		log("INFO", text);		
+	}
+
+}
